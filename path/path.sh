29682fb7c5075a0bc1d18d2837e79e7e55a736b8
Bryce Smith <bryces@umich.edu>
2019-06-12 10:59:31 -0400

    change to gt

--- path/path.sh
+++ path/path.sh
@@ -1,6 +1,6 @@
 # returns the CWD in windows format
 function winpath() {
-  if [ "$#" -ne 1 ]; then
+  if [ "$#" -gt 1 ]; then
     echo $1 | sed 's/\//\\/g'
   else
     sed 's/\//\\/g' <(pwd) | sed 's/\\mnt\\c\\/C\:\\/g'
